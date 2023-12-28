5139a2a068dcec6c44cb92bd897868bff21de223
Hans Perez <g00387884@atu.ie>
2023-12-28 16:05:19 +0000

    remove profile model

--- src/models/user.py
+++ src/models/user.py
@@ -1,10 +1,12 @@
-from sqlmodel import SQLModel, Field, Relationship
-from .book import Book
-
-
+from sqlmodel import SQLModel, Field
 
 
 class UserBase(SQLModel):
+    first_name: str = Field(gt=1, lt=200)
+    last_name: str = Field(gt=1, lt=200)
+    role: str
+    address: str = Field(gt=1, lt=200)
+    age: int = Field(ge=1, lt=200)
     email: str = Field(unique=True, index=True, gt=1, lt=200, regex="^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
     password: str = Field(gt=8, lt=200)
 
@@ -13,20 +15,4 @@ class User(UserBase, table=True):
 
 
 class UserCreate(UserBase):
-    pass
-
-class ProfileBase(SQLModel):
-    first_name: str = Field(gt=1, lt=200)
-    last_name: str = Field(gt=1, lt=200)
-    age: int = Field(ge=1, lt=200)
-
-
-class Profile(ProfileBase, table=True):
-    id: int = Field(default=None, primary_key=True)
-    user_id: int = Field(foreign_key="user.id", nullable=False, index=True)
-    role: str
-    address: str = Field(gt=1, lt=200)
-    user: User = Relationship(back_populates="profile")
-
-class ProfileCreate(ProfileBase):
     pass
