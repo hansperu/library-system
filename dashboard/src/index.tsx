5ba752182f1dee4fb3be5a30a9d5f7137a82dfd5
Hans Perez <g00387884@atu.ie>
2023-12-30 23:00:37 +0000

    feat: add home

--- dashboard/src/index.tsx
+++ dashboard/src/index.tsx
@@ -1,9 +1,21 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
+import { Router, Route } from '@solidjs/router'
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
+import { NotFound } from './pages/NotFound'
+import { Signup } from './pages/Signup'
 
 import './index.css'
-import App from './App'
 
 const root = document.getElementById('root')
 
-render(() => <App />, root!)
+render(() => (
+    <Router>
+        <Route path="/" component={Home}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/signup" component={Signup}/>
+        {/* <Route path={"/user/:id"} component={User}/> */}
+        <Route path="*404" component={NotFound}/>
+    </Router>
+), root!)
---
+++ dashboard/src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import type { Component } from 'solid-js'
+
+
+
+
+export const Home: Component = () => {
+    return (
+        <main class="hero min-h-screen" style="background-image: url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi2-prod.mirror.co.uk%2Fincoming%2Farticle13150515.ece%2FALTERNATES%2Fs1227b%2F0_PAY-Clock-over-door-to-Honby-library-Liverpool-central-library-Picton-reading-rooms.jpg);">
+            <div class="hero-overlay bg-opacity-60"></div>
+            <div class="hero-content text-center text-neutral-content">
+                <div class="max-w-md">
+                    <h1 class="mb-5 text-5xl font-bold">Welcome to the library system</h1>
+                    <p class="mb-5">Dive into an extensive library at your fingertips. Welcome to the vast collection of books from various authors. Whether you're a classic literature enthusiast or a fan of contemporary fiction, our platform caters to all your reading preferences.</p>
+                    <a class="btn btn-primary" href="/login">Get Started</a>
+                </div>
+            </div>
+        </main>
+    )
+}
